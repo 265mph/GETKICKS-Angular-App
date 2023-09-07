1580b9b8edfd0a46565178944f4732a5e3ccf853
Aina Temiloluwa Samuel <samuelaina18@gmail.com>
2023-09-07 16:53:31 +0100

    dded more angular forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { HomeSectionComponent } from './home-section/home-section.component';
-<<<<<<< HEAD
-=======
 import { SignupPageComponent } from './signup-page/signup-page.component';
->>>>>>> 6fbc6014a485f5675f74d1b9d5ad916680d675b5
 
 const routes: Routes = [
   {
@@ -16,14 +13,11 @@ const routes: Routes = [
   {
     path: '',
     component: HomeSectionComponent
-<<<<<<< HEAD
-=======
   },
   
   {
     path: 'Sign-Up',
     component: SignupPageComponent
->>>>>>> 6fbc6014a485f5675f74d1b9d5ad916680d675b5
   }
 ];
 
