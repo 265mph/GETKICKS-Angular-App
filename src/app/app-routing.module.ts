89ee5b41da6061d8bc97a63e276f99160909778d
Aina Temiloluwa Samuel <samuelaina18@gmail.com>
2024-05-16 15:52:14 +0100

    fixing routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginPageComponent } from './login-page/login-page.component';
 import { HomeSectionComponent } from './home-section/home-section.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 import { SignupPageComponent } from './signup-page/signup-page.component';
+import { ProductsComponent } from './products/products.component';
 
 const routes: Routes = [
   {
-    path: 'Login',
-    component: LoginPageComponent
+    path: '',
+    component: HomeSectionComponent
   },
 
   {
-    path: '',
-    component: HomeSectionComponent
+    path: 'Login',
+    component: LoginPageComponent
   },
   
   {
     path: 'Sign-Up',
     component: SignupPageComponent
+  },
+
+  {
+    path: 'Products',
+    component: ProductsComponent
   }
 ];
 
